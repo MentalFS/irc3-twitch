5eea4f494f5a73ec8b257c0118f0cfac9ef94472
MentalFS <git@mentalfs.de>
2017-12-18 13:00:17 +0100

    Add module that enables Twitch tags

---
+++ twitch_capabilities.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+from irc3.plugins.command import command
+import irc3
+
+@irc3.plugin
+class Plugin:
+
+	def __init__(self, bot):
+		self.bot = bot
+
+	def connection_made(self):
+		self.bot.send('CAP REQ :twitch.tv/membership')
+		self.bot.send('CAP REQ :twitch.tv/tags')
+		self.bot.send('CAP REQ :twitch.tv/commands')

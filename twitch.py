240eac30aa595f91a89025416ed812fd5c179c05
MentalFS <git@mentalfs.de>
2022-03-22 23:41:25 +0100

    twitch.py: Better handling for leaving channels

--- twitch.py
+++ twitch.py
@@ -5,6 +5,7 @@ import irc3
 class Access(object):
 	bot = None
 	channels = {}
+	users = {}
 
 	def __init__(self, bot):
 		self.bot = bot
@@ -17,21 +18,28 @@ class Twitch:
 		self.bot.twitch = self.access
 
 	def connection_made(self):
-		self.bot.send('CAP REQ :twitch.tv/membership')
+		#self.bot.send('CAP REQ :twitch.tv/membership')
 		self.bot.send('CAP REQ :twitch.tv/tags')
 		self.bot.send('CAP REQ :twitch.tv/commands')
 		self.access.channels = {}
+		self.access.users = {}
 
 
 	@irc3.event('@(\S+;)?room-id=(?P<id>\d+)(;\S+)? :\S+ ROOMSTATE #(?P<channelname>\S+)( :.*)?', iotype='in')
 	def on_roomstate_channel(self, id, channelname):
 		self.bot.log.debug('TWITCH ROOMSTATE: #%s - %s' % (channelname, id))
 		self.access.channels[id] = channelname
+		self.access.users[channelname] = id
 
 	@irc3.event('(@\S+ )?PART #(?P<channelname>\S+)( :.*)?', iotype='out')
 	def on_part_channel(self, channelname):
 		self.bot.log.debug('TWITCH PART: #%s' % channelname)
-		self.access.channels = { k:v for k, v in self.access.channels.items() if v != channelname }
+		#self.access.channels = { k:v for k, v in self.access.channels.items() if v != channelname }
+		if channelname in self.access.users:
+			id = self.access.users[channelname]
+			del self.access.users[channelname]
+			if id in self.access.channels:
+				del self.access.channels[id]
 
 	@irc3.event('(@\S+ )?:(?P<nickname>\S+)!\S+ PART #(?P<channelname>\S+)( :.*)?', iotype='in')
 	def on_part_channel_message(self, channelname, nickname):

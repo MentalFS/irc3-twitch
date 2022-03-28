05cd77ac95b61efdebb9e77c2efd21d5d26e4b81
MentalFS <git@mentalfs.de>
2022-03-28 19:23:43 +0200

    Twitch: Handle Whisper messages and NOTICE answers

--- twitch.py
+++ twitch.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-from irc3.plugins.command import command
+from irc3.plugins.command import Commands
 import irc3
 
 class Access(object):
@@ -10,8 +10,11 @@ class Access(object):
 	def __init__(self, bot):
 		self.bot = bot
 
+
 @irc3.plugin
 class Twitch:
+	requires = [Commands.__module__]
+
 	def __init__(self, bot):
 		self.bot = bot
 		self.access = Access(bot)
@@ -25,28 +28,50 @@ class Twitch:
 		self.access.users = {}
 
 
-	@irc3.event('@(\S+;)?room-id=(?P<id>\d+)(;\S+)? :\S+ ROOMSTATE #(?P<channelname>\S+)( :.*)?', iotype='in')
-	def on_roomstate_channel(self, id, channelname):
-		self.bot.log.debug('TWITCH ROOMSTATE: #%s - %s' % (channelname, id))
+	@irc3.event('@(\S+;)?room-id=(?P<channelid>\d+)(;\S+)? :\S+ ROOMSTATE #(?P<channelname>\S+)( :.*)?', iotype='in')
+	def on_roomstate_channel(self, channelid, channelname):
+		self.bot.log.debug('TWITCH ROOMSTATE: #%s - %s' % (channelname, channelid))
 		self.access.channels[id] = channelname
-		self.access.users[channelname] = id
+		self.access.users[channelname] = channelid
 
 	@irc3.event('(@\S+ )?PART #(?P<channelname>\S+)( :.*)?', iotype='out')
 	def on_part_channel(self, channelname):
 		self.bot.log.debug('TWITCH PART: #%s' % channelname)
 		if channelname in self.access.users:
-			id = self.access.users[channelname]
+			channelid = self.access.users[channelname]
 			del self.access.users[channelname]
-			if id in self.access.channels and self.access.channels[id] == channelname:
-				del self.access.channels[id]
+			if channelid in self.access.channels and self.access.channels[channelid] == channelname:
+				del self.access.channels[channelid]
 
 	@irc3.event('(@\S+ )?:(?P<nickname>\S+)!\S+ PART #(?P<channelname>\S+)( :.*)?', iotype='in')
 	def on_part_channel_message(self, channelname, nickname):
 		if nickname != self.bot.nick: return
 		self.on_part_channel(channelname)
 
+
 	@irc3.event('(@\S+ )?:\S+ RECONNECT( .*)?', iotype='in')
 	def on_reconnect_message(self, id, channelname):
 		self.bot.log.info('Twitch requested a reconnect.')
 		plugin = self.bot.get_plugin(irc3.utils.maybedotted('irc3.plugins.core.Core'))
 		self.bot.loop.call_soon(plugin.reconnect)
+
+
+	@irc3.event((r'(@(?P<tags>\S+) )?:(?P<mask>\S+) WHISPER (?P<target>\S+) '
+				 r':{re_cmd}(?P<cmd>[\w-]+)(\s+(?P<data>\S.*)|(\s*$))'))
+	def on_whisper_command(self, cmd, mask=None, target=None, client=None, **kw):
+		self.bot.log.debug('Received WHISPER COMMAND: %s' % cmd)
+		commands = self.bot.get_plugin(Commands)
+		commands.on_command(cmd, mask=mask, target=target, client=client, **kw)
+
+	@irc3.event('(@\S+ )?NOTICE (?P<target>\S+) :(?P<data>.*)', iotype='out')
+	def on_send_notice(self, target, data):
+		self.bot.log.debug('Repeated NOTICE as PRIVMSG: %s %s' % (target, data))
+		if target.startswith('#'):
+			self.bot.privmsg(target, data)
+		else:
+			self.bot.privmsg('#%s' % target, "/w %s %s" % (target, data))
+
+	@irc3.event('(@\S+ )?PRIVMSG (?P<user>[^#]\S+) :(?P<data>.*)', iotype='out')
+	def on_send_whisper(self, user, data):
+		self.bot.log.debug('Repeated PRIVMSG as WHISPER: /w %s %s' % (user, data))
+		self.bot.privmsg('#%s' % user, "/w %s %s" % (user, data))
